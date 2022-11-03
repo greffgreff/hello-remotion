7ba8a4b53a81d68b2075924c2282611f59035f9c
chandle greff <unconfigured@null.spigotmc.org>
2022-11-04 00:02:53 +0100

    Created hello animation

--- src/Video.tsx
+++ src/Video.tsx
@@ -1,19 +1,15 @@
 import {Composition} from 'remotion';
-import {HelloSkia} from './HelloSkia';
-
-// Each <Composition> is an entry in the sidebar!
+import {SkiaNeon} from './SkiaNeon';
 
 export const RemotionVideo: React.FC = () => {
 	return (
-		<>
-			<Composition
-				id="HelloSkia"
-				component={HelloSkia}
-				durationInFrames={150}
-				fps={30}
-				width={1920}
-				height={1080}
-			/>
-		</>
+		<Composition
+			id="video"
+			component={SkiaNeon}
+			durationInFrames={300}
+			fps={60}
+			width={1920}
+			height={1080}
+		/>
 	);
 };
